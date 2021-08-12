c8b75c74165fda213170265425c382cab3d6dfeb
Vojtech Mašek <vojtech.masek@flowup.cz>
2021-08-12 18:29:10 +0200

    perf: function resources bumped to the fastest option

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,5 +5,9 @@ import * as functions from 'firebase-functions';
 const ssrApp = require('./main').app();
 
 export const ssr = functions
-  .runWith({ timeoutSeconds: 10, memory: '2GB' })
+  .runWith({
+    timeoutSeconds: 5,
+    memory:
+      '4GB' /* 4096MB memory function will run at currently fastest - 4.8 GHz CPU ( https://cloud.google.com/functions/pricing) */,
+  })
   .https.onRequest(ssrApp);
