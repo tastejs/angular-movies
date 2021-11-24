779d8f23cf213ecfdb1de8e7d6c40884201237d6
Michael Hladky <michael@hladky.at>
2021-11-24 16:49:42 +0100

    chore: updat user flow cli

--- tooling/user-flows/commands/run-report.ts
+++ tooling/user-flows/commands/run-report.ts
@@ -1,5 +1,5 @@
 import { YargsCommandObject } from '../cli/model';
-import { report as coldWarmNavigationMainList, report } from '../flows/cold-warm-navigation--main-list';
+import { report as coldWarmNavigationMainList } from '../flows/cold-warm-navigation--main-list';
 import { report as coldWarmNavigationMovieDetail } from '../flows/cold-warm-navigation--movie-detail';
 import { report as categoryNavigation } from '../flows/category-to-category-cold-navigation';
 import { getCliParam } from '../cli/utils';
@@ -18,9 +18,8 @@ export const runCommand: YargsCommandObject = {
 
 
 export async function run(): Promise<void> {
-   const baseUrl: string = getCliParam(['targetUrl', 't']) || 'https://angular-movies-a12d3.web.app/';
+    const baseUrl: string = getCliParam(['targetUrl', 't']) || 'https://angular-movies-a12d3.web.app/';
 
-    console.log('baseUrl', baseUrl);
     await coldWarmNavigationMainList({baseUrl});
     await coldWarmNavigationMovieDetail({baseUrl, id: 566525});
     await categoryNavigation({baseUrl});
