a7091f933e580095d9f3c74859f9558ea0f9c75e
Michael Hladky <michael@hladky.at>
2021-11-29 20:26:33 +0100

    refactor: organize app.module imports

--- projects/movies/src/app/app.server.module.ts
+++ projects/movies/src/app/app.server.module.ts
@@ -1,23 +1,22 @@
 import { NgModule } from '@angular/core';
-import {
-  ServerModule,
-  ServerTransferStateModule,
-} from '@angular/platform-server';
+import { ServerModule, ServerTransferStateModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 import { RX_ANGULAR_CONFIG } from '@rx-angular/cdk';
 
 @NgModule({
-  imports: [AppModule, ServerModule, ServerTransferStateModule],
+  imports: [
+    AppModule, ServerModule, ServerTransferStateModule],
   providers: [
     {
       provide: RX_ANGULAR_CONFIG,
       useValue: {
-        primaryStrategy: 'native',
-      },
-    },
+        primaryStrategy: 'native'
+      }
+    }
   ],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppServerModule {}
+export class AppServerModule {
+}
