648837ce60df2ac9529cc8cef21480e2c732f625
Michael Hladky <michael@hladky.at>
2021-12-16 22:51:58 +0100

    fix naming

--- projects/movies/src/app/app.module.ts
+++ projects/movies/src/app/app.module.ts
@@ -4,10 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppShellModule } from './app-shell/app-shell.module';
-import { httpInterceptorProviders } from './data-access/auth/http-interceptor.providers';
-import { stateAppInitializerProvider } from './shared/state/state-app-initializer.provider';
 import { MovieListPageModule } from './pages/movie-list-page/movie-list-page.module';
 import { ROUTING_IMPORTS } from './app.routing';
+import { HTTP_INTERCEPTORS_PROVIDER } from './data-access/auth/http-interceptor.providers';
 import { GLOBAL_STATE_APP_INITIALIZER_PROVIDER } from './shared/state/state.app-initializer.provider';
 import { SCHEDULED_APP_INITIALIZER_PROVIDER } from './shared/app-initializer/chunk.app-initializer.provider';
 import { SERVICE_WORKER_IMPORTS } from './shared/pwa/service-worker.imports';
@@ -26,16 +25,10 @@ import { RXA_PROVIDER } from './shared/rxa-custom/rxa.provider';
     SERVICE_WORKER_IMPORTS,
     AppShellModule,
     MovieListPageModule,
-    ROUTING_IMPORTS,
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: environment.production,
-      // Register the ServiceWorker as soon as the app is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    })
+    ROUTING_IMPORTS
   ],
   providers: [
-    httpInterceptorProviders,
+    HTTP_INTERCEPTORS_PROVIDER,
     /**
      * **🚀 Perf Tip for LCP, TTI:**
      *
