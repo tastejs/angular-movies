38eb2193ca9edeae5378048ab63aa80d22576746
Michael Hladky <michael@hladky.at>
2021-12-23 05:19:22 +0100

    refactor: removed child components in favour of HTML

--- projects/movies/src/app/ui/components/header/header.component.ts
+++ projects/movies/src/app/ui/components/header/header.component.ts
@@ -4,12 +4,10 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   selector: 'ds-header',
   template: `
     <div class="header">
-      <ds-title>
-        <ng-content select="[title]"></ng-content>
-      </ds-title>
-      <ds-subtitle>
+      <h1 class="title"><ng-content select="[title]"></ng-content></h1>
+      <h2 class="subtitle">
         <ng-content select="[subtitle]"></ng-content>
-      </ds-subtitle>
+      </h2>
     </div>
   `,
   styles: [
@@ -20,6 +18,24 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
       .header {
         margin-bottom: 2rem;
+      }
+
+      .title {
+        margin-bottom: 0.5rem;
+        color: var(--palette-text-primary);
+        font-size: 3rem;
+        font-weight: 300;
+        line-height: 1;
+        text-transform: uppercase;
+        letter-spacing: -0.5px;
+      }
+
+      .subtitle {
+        color: var(--palette-text-secondary);
+        font-size: 1.5rem;
+        font-weight: 700;
+        line-height: 1;
+        text-transform: uppercase;
       }
     `
   ],
