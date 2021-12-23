708d87b8ad4854d5dec3a23337ceb4664d913120
Julian Jandl <julian@jandl.wien>
2021-12-23 17:29:18 +0100

    refactor: reduce html container in movie-detail

--- projects/movies/src/app/ui/atoms/detail-grid/detail-grid.component.ts
+++ projects/movies/src/app/ui/atoms/detail-grid/detail-grid.component.ts
@@ -3,13 +3,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'ds-detail-grid',
   template: `
-    <div class="grid">
-      <div class="grid--item gradient">
-        <ng-content select="[detailGridMedia]"></ng-content>
-      </div>
-      <div class="grid--item media">
-        <ng-content select="[detailGridDescription]"></ng-content>
-      </div>
+    <div class="grid--item gradient">
+      <ng-content select="[detailGridMedia]"></ng-content>
+    </div>
+    <div class="grid--item media">
+      <ng-content select="[detailGridDescription]"></ng-content>
     </div>
   `,
   styleUrls: ['./detail-grid.component.scss'],
