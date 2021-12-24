76d5e4bbd23c62f2e8f1e5387633ee7b431ed711
Michael Hladky <michael@hladky.at>
2021-12-24 03:28:12 +0100

    refactor: cleanup color, display

--- projects/movies/src/app/ui/atoms/form-control/form-control.component.ts
+++ projects/movies/src/app/ui/atoms/form-control/form-control.component.ts
@@ -3,19 +3,7 @@ import { ChangeDetectionStrategy, Component, NgModule } from '@angular/core';
 @Component({
   selector: 'app-form-control',
   template: `<ng-content></ng-content>`,
-  styles: [
-    `
-      :host {
-        margin: 8px 0;
-        display: flex;
-        flex-direction: column;
-        align-items: flex-start;
-      }
-      :host > :global(label + *) {
-        margin-top: 4px;
-      }
-    `,
-  ],
+  styles: ['./form-control.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FormControlComponent {}
