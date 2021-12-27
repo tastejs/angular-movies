977130cd1377512bc98ebf95380c52fc21f90b07
Michael Hladky <michael@hladky.at>
2021-12-27 03:23:21 +0100

    merge master

--- projects/movies/src/app/ui/component/search-bar/search-bar.component.ts
+++ projects/movies/src/app/ui/component/search-bar/search-bar.component.ts
@@ -1,14 +1,6 @@
 import { DOCUMENT } from '@angular/common';
-import {
-  ChangeDetectionStrategy,
-  Component,
-  ElementRef,
-  Inject, Input,
-  OnInit,
-  Output,
-  ViewChild, ViewEncapsulation
-} from '@angular/core';
-import { filter, fromEvent, map, mapTo, merge, Observable, startWith, switchMap, tap, withLatestFrom } from 'rxjs';
+import { ChangeDetectionStrategy, Component, ElementRef, Inject, Input, OnInit, Output, ViewChild, ViewEncapsulation } from '@angular/core';
+import { filter, fromEvent, map, merge, Observable, startWith, switchMap, take, withLatestFrom } from 'rxjs';
 import { getActions } from '../../../shared/rxa-custom/actions';
 import { RxState } from '@rx-angular/state';
 import { coerceObservable } from '@rx-angular/cdk';
@@ -30,11 +22,11 @@ import { coerceObservable } from '@rx-angular/cdk';
         <app-search-icon></app-search-icon>
       </button>
       <input *rxLet="search$; let search"
-        aria-label="Search Input"
-        #searchInput [value]="search"
-        (change)="ui.searchChange(searchInput.value)"
-        placeholder="Search for a movie..."
-        class="input"
+             aria-label="Search Input"
+             #searchInput [value]="search"
+             (change)="ui.searchChange(searchInput.value)"
+             placeholder="Search for a movie..."
+             class="input"
       />
     </form>
   `,
@@ -46,16 +38,19 @@ import { coerceObservable } from '@rx-angular/cdk';
 export class SearchBarComponent implements OnInit {
   @ViewChild('searchInput') inputRef!: ElementRef<HTMLInputElement>;
   @ViewChild('form') formRef!: ElementRef<HTMLFormElement>;
-  get input() {
-    return this.inputRef.nativeElement;
-  }
 
   ui = getActions<{
     searchChange: string,
-    formClick: any,
-    outsideFormClick: any,
-    formSubmit: any
-  }>({searchChange: (value) => value || ''})
+    formClick: Event,
+    outsideFormClick: Event,
+    formSubmit: Event
+  }>({
+    searchChange: s => s + '',
+    formSubmit: e => {
+      e.preventDefault();
+      return e;
+    }
+  });
 
   @Input()
   set query(v: string | Observable<string>) {
@@ -68,54 +63,61 @@ export class SearchBarComponent implements OnInit {
     map(([_, search]) => search)
   );
 
-  private get outsideClick() {
+  private readonly closedFormClick$ = this.ui.formClick$.pipe(
+    withLatestFrom(this.state.select('open')),
+    filter(([_, opened]) => !opened)
+  );
+
+  private outsideClick(): Observable<Event> {
+    // any click on the page (we can't use the option `once:true` as we might get multiple false trigger)
     return fromEvent(this.document, 'click').pipe(
+      // forward if the form did NOT triggered the click
+      // means we clicked somewhere else in the page but the form
       filter(e => !this.formRef.nativeElement.contains(e.target as any))
-    )
+    );
   }
 
+  /**
+   * **🚀 Perf Tip for TBT, TTI:**
+   *
+   * We avoid `@HostListener('document')` as it would add an event listener on component bootstrap no matter if we need it or not.
+   * This obviously will not scale.
+   *
+   * To avoid this we only listen to document click events after we clicked on the closed form.
+   * If the needed event to close the form is received we stop listening to the document.
+   *
+   * This way we reduce the active event listeners to a minimum.
+   */
+  private readonly outsideOpenFormClick$ = this.closedFormClick$.pipe(
+    switchMap(() => this.outsideClick().pipe(take(1)))
+  );
 
-  private readonly nativeElement: HTMLElement = this.elementRef.nativeElement;
+  private readonly classList = this.elementRef.nativeElement.classList;
+
   constructor(
-    private state: RxState<{search: string, open: boolean}>,
+    private state: RxState<{ search: string, open: boolean }>,
     @Inject(ElementRef) private elementRef: ElementRef,
     @Inject(DOCUMENT) private document: Document
   ) {
-    this.state.connect('search', this.ui.searchChange$.pipe(
-      startWith('')
-    ))
+    this.state.set({ open: false });
   }
 
   ngOnInit() {
-    let opened = false;
-    this.state.hold(
-      merge(
-        this.ui.formClick$.pipe(
-          filter(() => !opened),
-          switchMap(() => this.outsideClick.pipe(
-            mapTo(false),
-            startWith(true)
-          ))
-        ),
-        this.ui.formSubmit$.pipe(
-          tap(e => e.preventDefault()),
-          mapTo(false)
-        )
-      ),
-      open => {
-        if (open) {
-          this.input.focus();
-        }
-        this.setOpened(open);
-        opened = open;
-      }
-    )
+    this.state.hold(this.state.select('open'), this.setOpenedStyling);
+    this.state.hold(this.closedFormClick$, this.focusInput);
+
+    this.state.connect('search', this.ui.searchChange$.pipe(startWith('')));
+    this.state.connect('open', merge(this.ui.formSubmit$, this.outsideOpenFormClick$), () => false);
+    this.state.connect('open', this.closedFormClick$, () => true);
   }
 
-  private setOpened(opened: boolean) {
+  private readonly focusInput = () => {
+    return this.inputRef.nativeElement.focus();
+  };
+
+  private readonly setOpenedStyling = (opened: boolean) => {
     opened
-      ? this.nativeElement.classList.add('opened')
-      : this.nativeElement.classList.remove('opened');
-  }
-
+      ? this.classList.add('opened')
+      : this.classList.remove('opened');
+  };
 }
