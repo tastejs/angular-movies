fcf9badf703a504c95185941b3829d169937af2d
Julian Jandl <julian@jandl.wien>
2021-12-30 23:33:44 +0100

    refactor: make state working again :)

--- projects/movies/src/app/pages/movie-list-page/movie-list-page.adapter.ts
+++ projects/movies/src/app/pages/movie-list-page/movie-list-page.adapter.ts
@@ -1,4 +1,12 @@
-import { combineLatest, concatMap, map, Observable, Subject, switchMap, withLatestFrom } from 'rxjs';
+import {
+  combineLatest,
+  concatMap,
+  map,
+  Observable,
+  Subject,
+  switchMap,
+  withLatestFrom,
+} from 'rxjs';
 import { Injectable } from '@angular/core';
 import { MovieModel } from '../../data-access/model/movie.model';
 import { MovieGenreModel } from '../../data-access/model/movie-genre.model';
@@ -23,47 +31,44 @@ type MovieListPageModel = {
   activePage: number;
 };
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MovieListPageAdapter extends RxState<MovieListPageModel> {
-
   paginate$ = new Subject<void>();
 
-  routerSearch$ = this.routerState.select(getIdentifierOfTypeAndLayout('search', 'list'));
-  routerGenre$ = this.routerState.select(getIdentifierOfTypeAndLayout('genre', 'list'));
-  routerCategory$ = this.routerState.select(getIdentifierOfTypeAndLayout('category', 'list'));
+  routerSearch$ = this.routerState.select(
+    getIdentifierOfTypeAndLayout('search', 'list')
+  );
+  routerGenre$ = this.routerState.select(
+    getIdentifierOfTypeAndLayout('genre', 'list')
+  );
+  routerCategory$ = this.routerState.select(
+    getIdentifierOfTypeAndLayout('category', 'list')
+  );
 
   private readonly categoryMovieList$: Observable<MovieListPageModel> =
     this.movieState.select(
-      selectSlice([
-        'categoryMovies',
-        'categoryMoviesContext'
-      ]),
+      selectSlice(['categoryMovies', 'categoryMoviesContext']),
       withLatestFrom(this.routerCategory$),
-      map(
-        ([
-           { categoryMovies, categoryMoviesContext },
-           listName
-         ]) => {
-          return {
-            activePage: 1,
-            loading: categoryMoviesContext,
-            title: parseTitle(listName),
-            activeCategory: listName,
-            type: 'category',
-            movies: (categoryMovies && categoryMovies[listName]) || null
-          };
-        }
-      )
+      map(([{ categoryMovies, categoryMoviesContext }, listName]) => {
+        return {
+          activePage: 1,
+          loading: categoryMoviesContext,
+          title: parseTitle(listName),
+          activeCategory: listName,
+          type: 'category',
+          movies: (categoryMovies && categoryMovies[listName]) || null,
+        };
+      })
     );
 
   _slice$ = combineLatest({
-      genres: this.genreState.select('genres'),
-      discoverSlice: this.discoverState.select(selectSlice(['genreMovies', 'genreMoviesContext']))
-    }
-  ).pipe(
+    genres: this.genreState.select('genres'),
+    discoverSlice: this.discoverState.select(
+      selectSlice(['genreMovies', 'genreMoviesContext'])
+    ),
+  }).pipe(
     map(({ genres, discoverSlice }) => ({ genres, ...discoverSlice })),
     selectSlice(['genres', 'genreMovies', 'genreMoviesContext'])
   );
@@ -80,23 +85,36 @@ export class MovieListPageAdapter extends RxState<MovieListPageModel> {
         loading: genreMoviesContext,
         title: parseTitle(genreName),
         type: 'genre',
-        movies: (genreMovies && genreMovies[genreIdStr]) || null
+        movies: (genreMovies && genreMovies[genreIdStr]) || null,
       };
     })
   );
 
-  private readonly searchMovieList$: Observable<MovieListPageModel> = this.routerSearch$.pipe(
-    // cancel previous requests
-    switchMap((query) => getMoviesSearch(query)
-      .pipe(
-        map(({ results }) => ({ movies: results || null, title: parseTitle(query) }) as MovieListPageModel),
-        withLoadingEmission('loading', true, false)
+  private readonly searchMovieList$: Observable<MovieListPageModel> =
+    this.routerSearch$.pipe(
+      // cancel previous requests
+      switchMap((query) =>
+        getMoviesSearch(query).pipe(
+          map(
+            ({ results }) =>
+              ({
+                movies: results || null,
+                title: parseTitle(query),
+              } as MovieListPageModel)
+          ),
+          withLoadingEmission('loading', true, false)
+        )
       )
-    )
-  );
+    );
 
   private readonly routedMovieList$ = this.routerState.routerParams$.pipe(
-    switchMap(({ type }) => type === 'genre' ? this.genreMovieList$ : type === 'category' ? this.categoryMovieList$ : this.searchMovieList$)
+    switchMap(({ type }) =>
+      type === 'genre'
+        ? this.genreMovieList$
+        : type === 'category'
+        ? this.categoryMovieList$
+        : this.searchMovieList$
+    )
   );
 
   constructor(
@@ -112,19 +130,20 @@ export class MovieListPageAdapter extends RxState<MovieListPageModel> {
     this.connect(
       this.paginate$.pipe(
         withLatestFrom(this.routerCategory$),
-        concatMap(([_, cat]) => getMovieCategory(cat, this.get('activePage') + 1).pipe(
-          map(({ results }) => ({ movies: results }) as Partial<MovieListPageModel>),
-          withLoadingEmission(
-            'loading',
-            true,
-            false
-          )
+        concatMap(([_, cat]) =>
+          getMovieCategory(cat, this.get('activePage') + 1).pipe(
+            map(
+              ({ results }) =>
+                ({ movies: results } as Partial<MovieListPageModel>)
+            ),
+            withLoadingEmission('loading', true, false)
           )
         )
       ),
       (oldState, newSlice) => {
         if (newSlice.movies) {
-          return { movies: oldState.movies.concat(newSlice.movies) };
+          newSlice.movies = oldState.movies.concat(newSlice.movies);
+          newSlice.activePage = oldState.activePage + 1;
         }
         return newSlice;
       }
