13dec23375379deb6170970d02223d0b89fc4bbf
Michael Hladky <michael@hladky.at>
2022-01-12 04:35:04 +0100

    actions test 1

--- projects/movies/src/app/app.routing.ts
+++ projects/movies/src/app/app.routing.ts
@@ -33,7 +33,7 @@ const ROUTES: Routes = [
     component: MovieListPageComponent,
     /* loadChildren: () =>
       import('projects/movies/src/app/pages/movie-list-page/movie-list-page.module').then((m) => m.MovieListPageModule)
- */
+    */
   },
   {
     path: 'detail/movie/:identifier',
@@ -61,8 +61,6 @@ export const ROUTING_IMPORTS = [
      *
      * Disable initial sync navigation in router config and schedule it in router-outlet container component
      */
-    initialNavigation: 'disabled',
-    onSameUrlNavigation: 'reload',
-    relativeLinkResolution: 'legacy',
+    initialNavigation: 'disabled'
   }),
 ];
