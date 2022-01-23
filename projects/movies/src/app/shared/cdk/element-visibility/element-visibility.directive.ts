a319ac01db36dffecf86f277c0e09f3f7d2558a1
Vojtech Mašek <vojtech@flowup.cz>
2022-01-23 21:07:38 +0100

    formatting

--- projects/movies/src/app/shared/cdk/element-visibility/element-visibility.directive.ts
+++ projects/movies/src/app/shared/cdk/element-visibility/element-visibility.directive.ts
@@ -1,5 +1,12 @@
 import { isPlatformBrowser } from '@angular/common';
-import { Directive, ElementRef, Inject, OnDestroy, Output, PLATFORM_ID } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  Inject,
+  OnDestroy,
+  Output,
+  PLATFORM_ID,
+} from '@angular/core';
 import { getActions } from '../../rxa-custom/actions';
 import { observeElementVisibility } from './observe-element-visibility';
 import { takeUntil } from 'rxjs';
@@ -14,9 +21,11 @@ export class ElementVisibilityDirective implements OnDestroy {
   elementVisibility = this.signals.visible$;
 
   constructor(elRef: ElementRef, @Inject(PLATFORM_ID) platformId: Object) {
-    if (isPlatformBrowser(platformId)){ observeElementVisibility(elRef.nativeElement)
-      .pipe(takeUntil(this.signals.onDestroy$))
-      .subscribe(this.signals.visible);}
+    if (isPlatformBrowser(platformId)) {
+      observeElementVisibility(elRef.nativeElement)
+        .pipe(takeUntil(this.signals.onDestroy$))
+        .subscribe(this.signals.visible);
+    }
   }
 
   ngOnDestroy(): void {
