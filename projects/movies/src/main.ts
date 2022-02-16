b54054e191b83ab1ff594b2b509b2048b24aa724
Michael Hladky <michael@hladky.at>
2022-02-16 13:23:00 +0100

    perf: test boot on interaction

--- projects/movies/src/main.ts
+++ projects/movies/src/main.ts
@@ -7,30 +7,34 @@ import { environment } from './environments/environment';
 if (environment.production) {
   enableProdMode();
 }
-
-/**
- * **🚀 Perf Tip for TBT:**
- *
- * Schedule app bootstrap into next task to reduce Total Blocking Time (TTB).
- * We dont want to trigger style recalculation we avoid `animationFrame`.
- */
-setTimeout(() =>
-  platformBrowserDynamic()
-  /**
-   * **🚀 Perf Tip for LCP, TTI, TBT:**
-   *
-   * Disable zone.js as change detection system.
-   * Add { ngZone: 'noop' } to the bootstrap options
-   *
-   * ⚠ Notice:
-   * Don't forget to:
-   * - remove `zone.js` import from the `polyfills.ts` file
-   * - trigger change detection manually after NavigationEnd (or use the provided helper `ZonelessRouting`)
-   *
-   * 💡 Additional Optimization:
-   * Remove the `polyfills` option from your `angular.json` to save 1 request and 118b
-   *
-   */
-  .bootstrapModule(AppModule, { ngZone: 'noop' })
-    .catch((err) => console.error(err))
+document.addEventListener(
+  'click',
+  () =>
+    /**
+     * **🚀 Perf Tip for TBT:**
+     *
+     * Schedule app bootstrap into next task to reduce Total Blocking Time (TTB).
+     * We dont want to trigger style recalculation we avoid `animationFrame`.
+     */
+    setTimeout(() =>
+      platformBrowserDynamic()
+        /**
+         * **🚀 Perf Tip for LCP, TTI, TBT:**
+         *
+         * Disable zone.js as change detection system.
+         * Add { ngZone: 'noop' } to the bootstrap options
+         *
+         * ⚠ Notice:
+         * Don't forget to:
+         * - remove `zone.js` import from the `polyfills.ts` file
+         * - trigger change detection manually after NavigationEnd (or use the provided helper `ZonelessRouting`)
+         *
+         * 💡 Additional Optimization:
+         * Remove the `polyfills` option from your `angular.json` to save 1 request and 118b
+         *
+         */
+        .bootstrapModule(AppModule, { ngZone: 'noop' })
+        .catch((err) => console.error(err))
+    ),
+  { capture: true }
 );
