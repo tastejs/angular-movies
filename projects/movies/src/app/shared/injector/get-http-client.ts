d05389a3ca23c825e59965b9fe4d37b858e39574
Vojtech Mašek <vojtech@flowup.cz>
2022-03-27 00:14:19 +0100

    refactor: removed unnecessary injector getter helpers

--- projects/movies/src/app/shared/injector/get-http-client.ts
+++
@@ -1,8 +0,0 @@
-import { HttpClient } from '@angular/common/http';
-import { getInjector } from './root-injector.module';
-
-let _http: HttpClient;
-
-export function getHTTP() {
-  return _http || (_http = getInjector().get(HttpClient));
-}
