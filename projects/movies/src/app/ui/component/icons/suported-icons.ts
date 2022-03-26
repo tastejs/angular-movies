45d43ce999d78ba83b48e919731aed36d016a3c0
Vojtech Mašek <vojtech@flowup.cz>
2022-03-26 22:38:59 +0100

    refactor: icon registry dynamic import

---
+++ projects/movies/src/app/ui/component/icons/suported-icons.ts
@@ -0,0 +1,17 @@
+export type SupportedIcons = typeof SUPPORTED_ICONS[number];
+
+export const SUPPORTED_ICONS = [
+  'account',
+  'back',
+  'genre',
+  'imdb',
+  'play',
+  'popular',
+  'search',
+  'top_rated',
+  'upcoming',
+  'website',
+  'delete',
+  'sad',
+  'error',
+] as const;
