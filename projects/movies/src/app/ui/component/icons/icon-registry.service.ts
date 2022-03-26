7e17c1fef68d0116114276a5cc8658a1f6dce2bf
Michael Hladky <michael@hladky.at>
2022-03-26 16:52:00 +0100

    feat: use browser features to control priority and in future leverage lazy loading

--- projects/movies/src/app/ui/component/icons/icon-registry.service.ts
+++ projects/movies/src/app/ui/component/icons/icon-registry.service.ts
@@ -2,14 +2,14 @@ import { DOCUMENT } from '@angular/common';
 import { Inject, Injectable, Optional } from '@angular/core';
 import {
   catchError,
-  map,
-  merge,
   Observable,
+  of,
   shareReplay,
+  switchMap,
   throwError,
 } from 'rxjs';
-import { getHTTP } from '../../../shared/injector/get-http-client';
 
+const iconProvider = (name: string) => `/assets/svg-icons/${name}.svg`;
 const icons = [
   'account',
   'back',
@@ -25,6 +25,7 @@ const icons = [
   'sad',
   'error',
 ];
+icons;
 
 // credits go here: https://github.com/czeckd/angular-svg-icon/blob/master/projects/angular-svg-icon/src/lib/svg-icon-registry.service.ts
 
@@ -38,41 +39,40 @@ export class IconRegistry {
     @Optional() @Inject(DOCUMENT) private document: Document
   ) {
     // preload all icons
-    merge(
-      ...icons.map((i) => this.loadSvg(`/assets/svg-icons/${i}.svg`, i))
-    ).subscribe();
+    // merge(...icons.map((i) => this.loadSvg(i))).subscribe();
   }
 
-  loadSvg(
-    url: string,
-    name: string
-  ): Observable<SVGElement | undefined> | undefined {
+  loadSvg(name: string): Observable<SVGElement> {
+    const url = iconProvider(name);
     if (this._iconMap.has(name)) {
-      return this._iconMap.get(name);
+      return this._iconMap.get(name) as Observable<SVGElement>;
     }
-    const o$ = getHTTP()
-      .get(url, { responseType: 'text' })
-      .pipe(
-        map((svg) => {
-          const div = this.document.createElement('DIV');
-          div.innerHTML = svg;
-          return div.querySelector('svg') as SVGElement;
-        }),
-        catchError((err) => {
-          console.error(err);
-          return throwError(err);
-        }),
-        shareReplay(1)
-      ) as Observable<SVGElement>;
+    this.document.querySelector(`[data-svg-icon="${name}"]`);
+    const div = this.document.createElement('DIV');
+    div.innerHTML = `<svg viewBox="0 0 24 24">
+                        <use xlink:href='${url}#${name}'></use>
+                     </svg>`;
+    const svgElem = div.querySelector('svg') as SVGElement;
+    const o$ = of(svgElem).pipe(
+      catchError((err) => {
+        console.error(err);
+        return throwError(err);
+      }),
+      shareReplay(1)
+    ) as Observable<SVGElement>;
     this._iconMap.set(name, o$);
     return o$;
   }
 
   /** Get loaded SVG from registry by name. (also works by url because of blended map) */
-  getSvgByName(name: string): Observable<SVGElement | undefined> | undefined {
-    if (this._iconMap.has(name)) {
-      return this._iconMap.get(name);
-    }
-    return throwError(() => `No svg with name '${name}' has been loaded`);
+  getSvgByName(name: string): Observable<SVGElement> {
+    return new Observable<boolean>((s) => s.next(this._iconMap.has(name))).pipe(
+      switchMap((hasName) =>
+        hasName
+          ? (this._iconMap.get(name) as Observable<SVGElement>)
+          : this.loadSvg(name)
+      )
+      // @TODO handle errors
+    );
   }
 }
