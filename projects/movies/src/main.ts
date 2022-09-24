996e93d96d8bb15e2e476d89dec7eb88a8d07123
Michael Hladky <michael@hladky.at>
2022-09-24 14:33:42 +0200

    feat: make zone-full again to refactor standalone first

--- projects/movies/src/main.ts
+++ projects/movies/src/main.ts
@@ -32,6 +32,6 @@ document.addEventListener('DOMContentLoaded', () =>
      * Remove the `polyfills` option from your `angular.json` to save 1 request and 118b
      *
      */
-    .bootstrapModule(AppModule, { ngZone: 'noop' })
+    .bootstrapModule(AppModule /*{ ngZone: 'noop' }*/)
     .catch((err) => console.error(err))
 );
