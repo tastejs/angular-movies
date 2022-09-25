cd442675330c0fde2dc74e33f2cfe019a84ecb4b
Michael Hladky <michael@hladky.at>
2022-09-25 04:30:39 +0200

    feat: update rxIf

--- projects/movies/src/app/shared/rxa-custom/if/src/lib/if.directive.ts
+++ projects/movies/src/app/shared/rxa-custom/if/src/lib/if.directive.ts
@@ -1,16 +1,26 @@
 import {
   ChangeDetectorRef,
   Directive,
-  ElementRef,
   EmbeddedViewRef,
   Input,
   NgZone,
+  OnChanges,
   OnDestroy,
   OnInit,
   SimpleChanges,
   TemplateRef,
   ViewContainerRef,
 } from '@angular/core';
+import { coerceAllFactory, coerceObservable } from '@rx-angular/cdk/coercing';
+import {
+  createTemplateNotifier,
+  RxNotificationKind,
+} from '@rx-angular/cdk/notifications';
+import { RxStrategyProvider } from '@rx-angular/cdk/render-strategies';
+import {
+  createTemplateManager,
+  RxTemplateManager,
+} from '@rx-angular/cdk/template';
 import {
   NextObserver,
   Observable,
@@ -24,49 +34,45 @@ import {
   rxIfTemplateNames,
   RxIfViewContext,
 } from './model/index';
-import { coerceBooleanProperty } from '@angular/cdk/coercion';
-import {
-  createTemplateManager,
-  RxTemplateManager,
-} from '@rx-angular/cdk/template';
-import { RxStrategyProvider } from '@rx-angular/cdk/render-strategies';
-import { coerceAllFactory, coerceObservable } from '@rx-angular/cdk/coercing';
-import {
-  createTemplateNotifier,
-  RxNotificationKind,
-} from '@rx-angular/cdk/notifications';
 
 @Directive({
   selector: '[rxIf]',
 })
 // eslint-disable-next-line @angular-eslint/directive-class-suffix
-export class RxIf<U> implements OnInit, OnDestroy {
+export class RxIf<U> implements OnInit, OnChanges, OnDestroy, OnChanges {
   private subscription = new Subscription();
-  private _renderObserver: NextObserver<any> | undefined;
-  private templateManager:
-    | RxTemplateManager<U, RxIfViewContext<U>, rxIfTemplateNames>
-    | undefined;
+  // @ts-ignore
+  private _renderObserver: NextObserver<any>;
+  // @ts-ignore
+  private templateManager: RxTemplateManager<
+    U,
+    RxIfViewContext<U>,
+    rxIfTemplateNames
+  >;
 
-  @Input()
-  set rxIf(potentialObservable: Observable<U> | U | null | undefined) {
-    // @ts-ignore
-    this.observablesHandler.next(coerceObservable(potentialObservable));
-  }
+  // @ts-ignore
+  private lastValidValue: U | null;
+  @Input() rxIf: Observable<U> | U | null | undefined;
 
-  /* eslint-disable @angular-eslint/no-input-rename */
   @Input('rxIfStrategy')
   set strategy(strategyName: Observable<string> | string | null | undefined) {
     // @ts-ignore
     this.strategyHandler.next(strategyName);
   }
 
-  @Input('rxIfElse') else: TemplateRef<any> | undefined;
+  // @ts-ignore
+  @Input('rxIfElse') else: TemplateRef<RxIfViewContext<U>>;
+  // @ts-ignore
+  @Input('rxIfThen') then: TemplateRef<RxIfViewContext<U>>;
 
-  @Input('rxIfSuspenseTpl') suspenseTmpl: TemplateRef<any> | undefined;
-  @Input('rxIfCompleteTpl') completeTmpl: TemplateRef<any> | undefined;
-  @Input('rxIfErrorTpl') errorTmpl: TemplateRef<any> | undefined;
+  // @ts-ignore
+  @Input('rxIfSuspense') suspense: TemplateRef<RxIfViewContext<U>>;
+  // @ts-ignore
+  @Input('rxIfComplete') complete: TemplateRef<RxIfViewContext<U>>;
+  // @ts-ignore
+  @Input('rxIfError') error: TemplateRef<RxIfViewContext<U>>;
 
-  @Input('rxIfParent') renderParent = true;
+  @Input('rxIfParent') renderParent = this.strategyProvider.config.parent;
 
   @Input('rxIfPatchZone') patchZone = this.strategyProvider.config.patchZone;
 
@@ -76,10 +82,7 @@ export class RxIf<U> implements OnInit, OnDestroy {
   }
 
   /** @internal */
-  private observablesHandler = createTemplateNotifier<U>(
-    () => !!this.suspenseTmpl
-  );
-
+  private observablesHandler = createTemplateNotifier<U>();
   private readonly strategyHandler = coerceAllFactory<string>(
     // @ts-ignore
     () => new ReplaySubject<string | Observable<string>>(1),
@@ -87,18 +90,27 @@ export class RxIf<U> implements OnInit, OnDestroy {
   );
   private readonly rendered$ = new Subject<void>();
 
+  private get thenTemplate(): TemplateRef<RxIfViewContext<U>> {
+    return this.then ? this.then : this.templateRef;
+  }
+
   constructor(
     private strategyProvider: RxStrategyProvider,
     private cdRef: ChangeDetectorRef,
-    private eRef: ElementRef<Comment>,
     private ngZone: NgZone,
-    private readonly thenTemplateRef: TemplateRef<any>,
+    private readonly templateRef: TemplateRef<RxIfViewContext<U>>,
     private readonly viewContainerRef: ViewContainerRef
   ) {}
 
   ngOnInit() {
     this.subscription.add(
-      // @ts-ignore
+      this.observablesHandler.values$.subscribe(({ kind, value }) => {
+        if (kind === 'next' && value !== undefined) {
+          this.lastValidValue = value;
+        }
+      })
+    );
+    this.subscription.add(
       this.templateManager
         .render(this.observablesHandler.values$)
         .subscribe((n) => {
@@ -114,35 +126,39 @@ export class RxIf<U> implements OnInit, OnDestroy {
       this._createTemplateManager();
     }
 
+    if (changes.then && !changes.then.firstChange) {
+      this.templateManager.addTemplateRef(
+        RxIfTemplateNames.then,
+        this.thenTemplate
+      );
+    }
+
     if (changes.else) {
-      // @ts-ignore
       this.templateManager.addTemplateRef(RxIfTemplateNames.else, this.else);
     }
 
-    if (changes.completeTmpl) {
-      // @ts-ignore
+    if (changes.complete) {
       this.templateManager.addTemplateRef(
         RxIfTemplateNames.complete,
-        // @ts-ignore
-        this.completeTmpl
+        this.complete
       );
     }
 
-    if (changes.suspenseTmpl) {
-      // @ts-ignore
+    if (changes.suspense) {
       this.templateManager.addTemplateRef(
         RxIfTemplateNames.suspense,
-        // @ts-ignore
-        this.suspenseTmpl
+        this.suspense
       );
     }
 
-    if (changes.errorTmpl) {
-      // @ts-ignore
-      this.templateManager.addTemplateRef(
-        RxIfTemplateNames.error,
+    if (changes.error) {
+      this.templateManager.addTemplateRef(RxIfTemplateNames.error, this.error);
+    }
+
+    if (changes.rxIf) {
+      this.observablesHandler.next(
         // @ts-ignore
-        this.errorTmpl
+        this.rxIf === undefined ? this.rxIf : coerceObservable(this.rxIf)
       );
     }
   }
@@ -152,6 +168,14 @@ export class RxIf<U> implements OnInit, OnDestroy {
   }
 
   private _createTemplateManager(): void {
+    // @ts-ignore
+    const getNextTemplate = (value) => {
+      return value
+        ? RxIfTemplateNames.then
+        : this.else
+        ? RxIfTemplateNames.else
+        : undefined;
+    };
     this.templateManager = createTemplateManager<
       U,
       RxIfViewContext<U>,
@@ -167,30 +191,34 @@ export class RxIf<U> implements OnInit, OnDestroy {
       },
       renderSettings: {
         cdRef: this.cdRef,
-        eRef: this.eRef,
         parent: coerceBooleanProperty(this.renderParent),
         patchZone: this.patchZone ? this.ngZone : false,
         defaultStrategyName: this.strategyProvider.primaryStrategy,
         strategies: this.strategyProvider.strategies,
       },
       notificationToTemplateName: {
-        [RxNotificationKind.Suspense]: () => RxIfTemplateNames.suspense,
+        [RxNotificationKind.Suspense]: () =>
+          // @ts-ignore
+          this.suspense
+            ? RxIfTemplateNames.suspense
+            : getNextTemplate(this.lastValidValue),
         // @ts-ignore
-        [RxNotificationKind.Next]: (value, templates) => {
-          return value
-            ? (RxIfTemplateNames.then as rxIfTemplateNames)
-            : // @ts-ignore
-            templates.get(RxIfTemplateNames.else)
-            ? RxIfTemplateNames.else
-            : undefined;
-        },
-        [RxNotificationKind.Error]: () => RxIfTemplateNames.error,
-        [RxNotificationKind.Complete]: () => RxIfTemplateNames.complete,
+        [RxNotificationKind.Next]: getNextTemplate.bind(this),
+        [RxNotificationKind.Error]: () =>
+          // @ts-ignore
+          this.error
+            ? RxIfTemplateNames.error
+            : getNextTemplate(this.lastValidValue),
+        [RxNotificationKind.Complete]: () =>
+          // @ts-ignore
+          this.complete
+            ? RxIfTemplateNames.complete
+            : getNextTemplate(this.lastValidValue),
       },
     });
     this.templateManager.addTemplateRef(
       RxIfTemplateNames.then,
-      this.thenTemplateRef
+      this.thenTemplate
     );
     // @ts-ignore
     this.templateManager.nextStrategy(this.strategyHandler.values$);
@@ -201,14 +229,15 @@ function createViewContext<T>(value: T): RxIfViewContext<T> {
   return {
     rxIf: value,
     $implicit: value,
-    $error: false,
-    $complete: false,
-    $suspense: false,
+    error: false,
+    complete: false,
+    suspense: false,
   };
 }
 
 function updateViewContext<T>(
-  _: T,
+  // @ts-ignore
+  value: T,
   view: EmbeddedViewRef<RxIfViewContext<T>>,
   context: RxIfViewContext<T>
 ): void {
@@ -216,4 +245,11 @@ function updateViewContext<T>(
     // @ts-ignore
     view.context[k] = context[k];
   });
+}
+
+/**
+ * Coerces a data-bound value (typically a string) to a boolean.
+ */
+function coerceBooleanProperty(value: any): boolean {
+  return value != null && `${value}` !== 'false';
 }
