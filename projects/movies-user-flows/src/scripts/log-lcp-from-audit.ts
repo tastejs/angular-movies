2fafeb442e44df7fc1e124d7bdfe9244881a8563
Michael Hladky <michael@hladky.at>
2022-12-17 23:25:23 +0100

    feat: log audit details

--- projects/movies-user-flows/src/scripts/log-lcp-from-audit.ts
+++ projects/movies-user-flows/src/scripts/log-lcp-from-audit.ts
@@ -1,16 +1,20 @@
-// import * as fs from 'fs';
+import * as fs from 'fs';
+import * as path from 'path';
 console.log('run LCP experiment');
-/*(() => {
+(() => {
   const { collect, persist } = JSON.parse(
-    fs.readFileSync('./user-flowrc.lcp.json', 'uft8') || '{}'
+    fs.readFileSync('./projects/movies-user-flows/.user-flowrc.lcp.json', {
+      encoding: 'utf8',
+    }) || '{}'
   );
   const { outPath } = persist;
 
   const f = fs.readdirSync(outPath).find((n) => n.includes('lcp'));
 
   if (f) {
-    let reportJson = JSON.parse(fs.readFileSync(f, 'utf8'));
-    console.log(reportJson);
+    let reportJson = JSON.parse(
+      fs.readFileSync(path.join(outPath, f), { encoding: 'utf8' })
+    );
+    console.log(reportJson.steps[0].lhr.audits['lcp-lazy-loaded'].details);
   }
 })();
-*/
