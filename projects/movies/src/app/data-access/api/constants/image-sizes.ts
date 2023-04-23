ab7c61c8a80411cce3292ac536be393910c15a02
Michael <michael.hladky@push-based.io>
2023-04-23 12:22:33 +0200

    refactor: add breakpoints and sizes

--- projects/movies/src/app/data-access/api/constants/image-sizes.ts
+++ projects/movies/src/app/data-access/api/constants/image-sizes.ts
@@ -1,14 +1,20 @@
 // @Notice: This information could get fetched from the TMDB API
 import { ImageDimensions } from './image-dimensions.interface';
-/*
-w92
-w154
-w185
-w342
-w500
-w780
-original
-*/
+
+export const SIZES = {
+  "92w": `92w`,
+  "154w": `154w`,
+  "185w": `185w`,
+  "342w":  `342w`,
+  "500w":  `500w`,
+  "780w": `780w`
+};
+
+export const W92H138: ImageDimensions = {
+  SIZE: '92w',
+  WIDTH: 92,
+  HEIGHT: 138,
+} as const;
 
 export const W154H205: ImageDimensions = {
   SIZE: '154w',
@@ -20,24 +26,11 @@ export const W185H278: ImageDimensions = {
   WIDTH: 185,
   HEIGHT: 278,
 } as const;
-
 export const W300H450: ImageDimensions = {
   SIZE: '300w',
   WIDTH: 300,
   HEIGHT: 450,
 } as const;
-/*
-export const W342H513: ImageDimensions = {
-  WIDTH: 342,
-  HEIGHT: 513,
-} as const;
-*/
-export const W780H1170: ImageDimensions = {
-  SIZE: '780w',
-  WIDTH: 780,
-  HEIGHT: 1170,
-} as const;
-
 
 export const W500H282: ImageDimensions = {
   SIZE: '500w',
@@ -45,26 +38,10 @@ export const W500H282: ImageDimensions = {
   HEIGHT: 282,
 } as const;
 
-export const W92H138: ImageDimensions = {
-  SIZE: '92w',
-  WIDTH: 92,
-  HEIGHT: 138,
-} as const;
-/*
-export const W355H200: ImageDimensions = {
-  WIDTH: 355,
-  HEIGHT: 200,
+export const W780H1170: ImageDimensions = {
+  SIZE: '780w',
+  WIDTH: 780,
+  HEIGHT: 1170,
 } as const;
 
-export const W355_AND_H200_BESTV2: ImageDimensions = {
-  FULL: 'w355_and_h200_bestv2',
-  WIDTH: 355,
-  HEIGHT: 200,
-} as const;
 
-export const W710_AND_H400_BESTV2: ImageDimensions = {
-  FULL: 'w700_and_h400_bestv2',
-  WIDTH: 700,
-  HEIGHT: 400,
-} as const;
-*/
