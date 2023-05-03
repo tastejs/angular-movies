84ab400d45bb45f45af9ad0fc36eb4c555603f49
Michael <michael.hladky@push-based.io>
2023-05-03 06:53:40 -0600

    test: profile tests wip 3

--- projects/movies-user-flows/src/ufo/desktop/tmdb.ufo.ts
+++ projects/movies-user-flows/src/ufo/desktop/tmdb.ufo.ts
@@ -8,22 +8,27 @@ export class TmdbUfo extends Ufo {
   }
 
   async fillLoginForm(): Promise<any> {
-    await this.page.waitForSelector(fixtures.TmdbUsernameInput);
+    await this.page.waitForSelector(fixtures.TmdbUsernameInput, {timeout: 15000});
     await this.page.type(fixtures.TmdbUsernameInput, fixtures.TmdbUser);
+
     await this.page.waitForSelector(fixtures.TmdbPasswordInput);
     await this.page.type(fixtures.TmdbPasswordInput, fixtures.TmdbPassword);
+
+    await this.page.waitForSelector(fixtures.TmdbLoginSubmitBtn);
     await this.page.click(fixtures.TmdbLoginSubmitBtn)
   }
 
   async login(): Promise<any> {
     await this.page.waitForSelector(fixtures.TmdbLoginBtn);
     await this.page.click(fixtures.TmdbLoginBtn);
+
     // fill and send form
     await this.fillLoginForm();
 
     // approve access
     await this.page.waitForSelector(fixtures.TmdbApproveBtn);
     await this.page.click(fixtures.TmdbApproveBtn);
+
     // navigate back to movies app
     await this.page.waitForNavigation().catch(() => {
       throw new Error('Navigation to movies app failed')
