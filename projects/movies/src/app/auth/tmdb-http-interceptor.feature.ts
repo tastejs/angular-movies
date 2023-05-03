edfc1170ec02d42bdc2b394d7c3d1b4c1e069bcb
Michael <michael.hladky@push-based.io>
2023-05-03 01:13:34 -0500

    refactor: format code

--- projects/movies/src/app/auth/tmdb-http-interceptor.feature.ts
+++ projects/movies/src/app/auth/tmdb-http-interceptor.feature.ts
@@ -5,21 +5,20 @@ import {
 } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { DOCUMENT } from '@angular/common';
+import {DOCUMENT} from "@angular/common";
 
 export const tmdbReadAccessInterceptor: HttpInterceptorFn = (
   req: HttpRequest<unknown>,
   next: HttpHandlerFn
 ) => {
+
   const document = inject(DOCUMENT);
-  const token =
-    document.defaultView?.localStorage?.accessToken ||
-    environment.tmdbApiReadAccessKey;
+  const token = document.defaultView?.localStorage?.accessToken || environment.tmdbApiReadAccessKey;
 
   return next(
     req.clone({
       setHeaders: {
-        Authorization: `Bearer ${token}`,
+        Authorization:`Bearer ${token}`
       },
     })
   );
