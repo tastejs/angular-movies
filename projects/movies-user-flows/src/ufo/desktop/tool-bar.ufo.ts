1c3ef4d71bf44b7d1dce0b19434594a3042cb9ac
Michael <michael.hladky@push-based.io>
2023-05-04 17:06:33 -0600

    test: wip account test 1

--- projects/movies-user-flows/src/ufo/desktop/tool-bar.ufo.ts
+++ projects/movies-user-flows/src/ufo/desktop/tool-bar.ufo.ts
@@ -1,14 +1,18 @@
 import {CwvInterface} from '../typings/cwv.interface';
 import * as fixtures from '../../fixtures/toolbar.fixtures';
+import * as tmdbfixtures from '../../fixtures/tmdb.fixtures';
 import {GenreIds} from '../../internals/typings';
 import {Ufo, UserFlowContext} from '@push-based/user-flow';
+import {TmdbUfo} from "./tmdb.ufo";
 
 export class ToolBarUfo extends Ufo implements CwvInterface {
+  tmdbPage: TmdbUfo;
   constructor(private ctx: UserFlowContext) {
     super(ctx);
+    this.tmdbPage = new TmdbUfo(ctx);
   }
 
-  async sendSerachForm() {
+  async sendSearchForm() {
     await this.page.keyboard.type(fixtures.searchSubmitKeys[0]);
   }
 
@@ -17,12 +21,38 @@ export class ToolBarUfo extends Ufo implements CwvInterface {
     await this.page.keyboard.type(query);
   }
 
-  async toggleDarkmode(g: GenreIds) {
+  async toggleDarkMode(g: GenreIds) {
     throw new Error('not implemented');
   }
 
-  async openAccountMenu(): Promise<any> {
-    throw new Error('not implemented');
+  async openProfileMenu(): Promise<any> {
+    await this.page.waitForSelector(fixtures.profileMenu);
+    await this.page.click(fixtures.profileMenu);
+    await this.page.waitForSelector(fixtures.profileMenuContent);
+  }
+
+
+  async goToTmDbLogin(): Promise<any> {
+    // open menu
+    await this.openProfileMenu();
+    // navigate to tmdb
+    await this.page.waitForSelector(fixtures.profileMenuLoginItem);
+    await this.page.click(fixtures.profileMenuLoginItem);
+
+    await this.page.waitForResponse(r => r.url().includes(tmdbfixtures.TmdbAuthUrl))
+      .catch(() => {
+        throw new Error('Navigation to tmdb failed')
+      });
+  }
+
+  async ensureLoginDone(): Promise<any> {
+    // open menu
+    await this.page.waitForResponse(r => r.url().includes('angular'))
+      .catch(() => {
+        throw new Error('Navigation back to movies app failed')
+      });
+    await this.openProfileMenu()
+    await this.page.waitForSelector(fixtures.profileMenuSignoutItem);
   }
 
   async awaitLCPContent(): Promise<any> {
