af6ab1be35e10074ad69ec74ff59e7a251efc485
Michael <michael.hladky@push-based.io>
2023-05-04 15:06:23 -0600

    test: profile tests wip 6

--- projects/movies-user-flows/src/fixtures/toolbar.fixtures.ts
+++ projects/movies-user-flows/src/fixtures/toolbar.fixtures.ts
@@ -5,4 +5,4 @@ export const searchSubmitKeys = ['Enter'];
 export const profileMenu = '*[data-uf="profile-menu-button"]';
 export const profileMenuContent = '*[data-uf="profile-menu-content"]';
 export const profileMenuLoginItem = '*[data-uf="profile-menu-item-login"]';
-export const TmdbAuthUrl = 'https://www.themoviedb.org/auth/access';
+export const profileMenuSignoutItem = '*[data-uf="profile-menu-item-signout"]';
