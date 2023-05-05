461308631025d3aafe4b5fb3d8741dce3d210e49
Michael <michael.hladky@push-based.io>
2023-05-05 14:16:01 -0600

    test: screenshots 1

--- projects/movies-user-flows/src/ufo/desktop/tmdb.ufo.ts
+++ projects/movies-user-flows/src/ufo/desktop/tmdb.ufo.ts
@@ -24,7 +24,7 @@ export class TmdbUfo extends Ufo {
     await this.page.click(fixtures.TmdbLoginSubmitBtn);
     await this.page.waitForTimeout(6000);
 
-    await this.page.screenshot().then(i => writeFileSync('./projects/movies-user-flows/src/measures/login-btn-clicked.jpg', i))
+    await this.page.screenshot().then(i => writeFileSync('./login-btn-clicked.jpg', i))
     await this.page.click(fixtures.TmdbCookieBannerBtn).catch(e => {
       console.log('no cookie banner here')
     });
@@ -34,7 +34,7 @@ export class TmdbUfo extends Ufo {
     await this.page.waitForSelector(fixtures.TmdbLoginSubmitBtn);
     await this.page.click(fixtures.TmdbLoginSubmitBtn);
     await this.page.waitForTimeout(6000);
-    await this.page.screenshot().then(i => writeFileSync('./projects/movies-user-flows/src/measures/approve-visible.jpg', i))
+    await this.page.screenshot().then(i => writeFileSync('.//approve-visible.jpg', i))
     // approve access
     // await this.page.waitForSelector(fixtures.TmdbApproveBtn);
     // await this.page.click(fixtures.TmdbApproveBtn);
