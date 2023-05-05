8f36801dc2f13cf16408604586124a9b38d90eb7
Michael <michael.hladky@push-based.io>
2023-05-05 16:38:08 -0600

    test: remove timespans

--- projects/movies-user-flows/src/user-flows/account-feature.uf.ts
+++ projects/movies-user-flows/src/user-flows/account-feature.uf.ts
@@ -33,15 +33,12 @@ const interactions: UserFlowInteractionsFn = async (
     }
   });
 
-  await flow.startTimespan('go to tmdb');
   await toolbar.goToTmDbLogin();
-  await flow.endTimespan();
 
   await tmdpPage.login();
 
-  await flow.startTimespan('go to angular movies');
   await toolbar.ensureLoginDone();
-  await flow.endTimespan();
+
 
   return Promise.resolve();
 };
