626dc06cc2f1fb131bd23df0e26fd727b890ba1b
Michael <michael.hladky@push-based.io>
2023-07-04 05:07:39 +0200

    refactor: move build logic

---
+++ projects/ng-universal-express/src/index.ts
@@ -0,0 +1,93 @@
+import 'zone.js/dist/zone-node';
+
+import {join} from 'path';
+import {existsSync} from 'fs';
+
+import express from 'express';
+// import serverTiming from 'server-timing';
+import compressionModule from 'compression';
+import {ngExpressEngine} from '@nguniversal/express-engine';
+
+import {ISRHandler} from 'ngx-isr';
+import {environment} from '../../movies/src/environments/environment';
+
+import bootstrap from '../../movies/src/main.server';
+
+// The Express app is exported so that it can be used by serverless Functions.
+export function app(): express.Express {
+  const server = express();
+
+  const distFolder = process.cwd().replace('/server', '/browser');
+
+  const indexHtml = existsSync(join(distFolder, 'index.html'))
+    ? 'index.html'
+    : 'index';
+
+  const isr = new ISRHandler({
+    indexHtml,
+    invalidateSecretToken: 'MY_TOKEN',
+    enableLogging: !environment.production,
+  });
+
+  // patchWindow(indexHtml);
+
+  // **🚀 Perf Tip:**
+  // Serve gzip for faster load
+  server.use(compressionModule());
+
+  // server.use(serverTiming());
+
+  // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
+  server.engine(
+    'html',
+    ngExpressEngine({bootstrap, inlineCriticalCss: false})
+  );
+
+  server.set('view engine', 'html');
+  server.set('views', distFolder);
+
+  // Example Express Rest API endpoints
+  // server.get('/api/**', (req, res) => { });
+  // Serve static files from /browser
+  server.get(
+    '*.*',
+    express.static(distFolder, {
+      maxAge: '1y',
+
+      // missing assets results in 404 instead of continuing to next route handler (and rendering route)
+      fallthrough: false,
+    })
+  );
+
+  server.get(
+    '*',
+    // Serve page if it exists in cache
+    async (req, res, next) => await isr.serveFromCache(req, res, next),
+    // Server side render the page and add to cache if needed
+    async (req, res, next) => await isr.render(req, res, next)
+  );
+
+  return server;
+}
+
+function run(): void {
+  const port = process.env.PORT || 4000;
+
+  // Start up the Node server
+  const server = app();
+  server.listen(port, () => {
+    console.log(`Node Express server listening on http://localhost:${port}`);
+  });
+}
+
+// Webpack will replace 'require' with '__webpack_require__'
+// '__non_webpack_require__' is a proxy to Node 'require'
+// The below code is to ensure that the server is run only when not requiring the bundle.
+declare const __non_webpack_require__: NodeRequire;
+const mainModule = __non_webpack_require__.main;
+const moduleFilename = (mainModule && mainModule.filename) || '';
+if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
+  run();
+}
+
+export default bootstrap;
