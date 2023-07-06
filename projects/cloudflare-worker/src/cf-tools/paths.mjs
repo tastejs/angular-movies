ea9f9679f5899f73de435fb1f8efb03122f43f06
Michael <michael.hladky@push-based.io>
2023-07-06 02:38:24 +0200

    refactor: ng configuration for firebase 2

--- projects/cloudflare-worker/src/cf-tools/paths.mjs
+++
@@ -1,9 +0,0 @@
-import path from "node:path";
-import {fileURLToPath} from "node:url";
-
-const dirname = path.dirname(fileURLToPath(import.meta.url));
-export const root = path.resolve(dirname, "..");
-export const client = path.resolve(root, "dist/projects/movies/browser");
-export const ssr = path.resolve(root, "dist/projects/movies/server");
-export const cloudflare = path.resolve(root, "dist/projects/movies/cloudflare");
-export const worker = path.resolve(cloudflare, "_worker.js");
