b8a36d0ed60f4f876e3205632c40bfe4f2e15a80
Michael <michael.hladky@push-based.io>
2023-07-06 21:24:47 +0200

    refactor: add new project files

---
+++ projects/movies-user-flows/README.md
@@ -0,0 +1,7 @@
+# movies-user-flows
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build movies-user-flows` to build the library.
---
+++ projects/movies-user-flows/src/index.ts
@@ -0,0 +1,4 @@
+export {SidebarUFO} from './ufo/mobile/side-bar.ufo';
+export {MovieDetailPageUFO} from './ufo/desktop/movie-detail-page.ufo';
+export {MovieListPageUFO} from './ufo/desktop/movie-list-page.ufo';
+export {mergeBudgets} from './internals/test-sets';
