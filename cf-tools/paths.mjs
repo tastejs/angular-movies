aab48e75c62045ae496a33582f863deb6ad6dec6
Michael <michael.hladky@push-based.io>
2023-07-08 07:13:26 +0200

    refactor: remove old files

--- cf-tools/paths.mjs
+++
@@ -1,9 +0,0 @@
-import path from "node:path";
-import {fileURLToPath} from "node:url";
-
-const dirname = path.dirname(fileURLToPath(import.meta.url));
-export const root = path.resolve(dirname, "..");
-export const client = path.resolve(root, "dist/projects/movies/browser");
-export const ssr = path.resolve(root, "dist/projects/movies/worker");
-export const cloudflare = path.resolve(root, "dist/projects/movies/worker");
-export const worker = path.resolve('dist', "_worker.js");
