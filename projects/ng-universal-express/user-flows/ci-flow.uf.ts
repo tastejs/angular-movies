972f5734769f2c37dd2f8d517652cdf81b16a5f6
ChristopherPHolder <toholderandrews@gmail.com>
2023-07-13 22:04:27 +0200

    fix: add ssr test

--- projects/ng-universal-express/user-flows/ci-flow.uf.ts
+++ projects/ng-universal-express/user-flows/ci-flow.uf.ts
@@ -5,17 +5,39 @@ const flowOptions: UserFlowOptions = {
   name: 'Ng Universal Pre-rendering',
 };
 
+// Verifies that the movies app is pre-rendered
 const interactions: UserFlowInteractionsFn = async (
   ctx: UserFlowContext
-): Promise<any> => {
-  const {flow, collectOptions} = ctx;
+): Promise<void> => {
+  const {flow, collectOptions, page} = ctx;
   const url = `${collectOptions.url}/list/category/popular`;
   const movieListPage = new MovieListPageUFO(ctx);
+
+  await page.setRequestInterception(true);
+
+  page.on('request', req => req.continue());
+
+  let initialResponse: Promise<string>;
+  page.on('response', response => {
+    if (response.url() === url) {
+      initialResponse = response.text();
+    }
+  });
 
   await flow.navigate(url, {
     stepName: '🧭 Initial navigation'
   });
+  const responseText = await initialResponse;
+
+  if (responseText.includes('loading-csr')) {
+    throw new Error('Loading Spinner is present in initial navigation');
+  }
+  if (!responseText.includes('data-uf="movie-0"')) {
+    throw new Error('Movie is not present in initial navigation');
+  }
+
   await movieListPage.awaitHeadingContent();
+
   return Promise.resolve();
 };
 
