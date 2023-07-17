65ff4229fdbf60ba4cee5e687da2c944046b3951
Michael <michael.hladky@push-based.io>
2023-07-17 18:36:18 +0200

    refactor: add person detail page

--- projects/movies/user-flows/person-details.uf.ts
+++ projects/movies/user-flows/person-details.uf.ts
@@ -5,18 +5,20 @@ import {getLhConfig} from "../../movies-user-flows/src/internals/test-sets";
 import * as angularBudgets from "../testing/budgets/angular.budgets.json";
 import * as generalTimingBudget from "../testing/budgets/general-timing.budgets.json";
 import * as movieListBudgets from "../testing/budgets/movie-list.budgets.json";
+import {PersonDetailPageUFO} from "../../movies-user-flows/src/ufo/desktop/person-detail-page.ufo";
 
 const flowOptions: UserFlowOptions = {
-  name: 'Movie Detail Tests',
+  name: 'Person Detail Tests',
 };
 
 const interactions: UserFlowInteractionsFn = async (
   ctx: UserFlowContext
 ): Promise<any> => {
-  const {flow, collectOptions} = ctx;
+  const {flow, collectOptions, page} = ctx;
   const url = `${collectOptions.url}/list/category/popular`;
   const movieListPage = new MovieListPageUFO(ctx);
   const movieDetailPage = new MovieDetailPageUFO(ctx);
+  const personDetailPage = new PersonDetailPageUFO(ctx);
 
   await flow.navigate(url, {
     stepName: '🧭 Initial navigation',
@@ -33,31 +35,35 @@ const interactions: UserFlowInteractionsFn = async (
 
   // ======= Detail navigations =======
   await flow.startTimespan({
-    stepName: '🧭 Navigate to detail page',
+    stepName: '🧭 Navigate to detail person',
   });
   await movieListPage.navigateToDetail(1);
   await movieDetailPage.awaitAllContent();
-  currentHeroImageSrc = await movieDetailPage.getHeroImageSrc();
+  await movieDetailPage.goToPersonDetail(1);
+  await personDetailPage.awaitAllContent();
+
+  currentHeroImageSrc = await personDetailPage.getHeroImageSrc();
   await flow.endTimespan();
   await flow.snapshot({
     stepName: '✔ Navigation to detail page done',
   });
 
-  // ======= Hero image change for recommendations =======
+  // ======= Hero image change for cast =======
 
   await flow.startTimespan({
-    stepName: '🧭 Navigate to other detail page',
+    stepName: '🧭 Navigate to cast',
   });
-  await movieListPage.navigateToDetail(1);
-  await movieDetailPage.awaitAllContent();
+  await movieDetailPage.goToPersonDetail(1);
+  await personDetailPage.awaitAllContent();
 
-  const recommendationImageSrc = await movieDetailPage.getHeroImageSrc();
-  if (recommendationImageSrc === currentHeroImageSrc) {
-    throw new Error("hero image does not change when navigating to recommended movie!")
+  const castImageSrc = await personDetailPage.getHeroImageSrc();
+  if (castImageSrc === currentHeroImageSrc) {
+    throw new Error("Hero image does not change when navigating to cast!")
   }
+
   await flow.endTimespan();
   await flow.snapshot({
-    stepName: '✔ Navigation to other detail page done',
+    stepName: '✔ Navigation to cast detail page done',
   });
 
   return Promise.resolve();
