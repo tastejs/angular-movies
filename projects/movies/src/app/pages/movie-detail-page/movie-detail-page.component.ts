d0f227888a57155a18328941253e82d355b8f632
jgelin <jgelin@seligent.com>
2023-07-18 08:08:48 +0200

    fix: image re-rendering for movie details

--- projects/movies/src/app/pages/movie-detail-page/movie-detail-page.component.ts
+++ projects/movies/src/app/pages/movie-detail-page/movie-detail-page.component.ts
@@ -1,5 +1,5 @@
-import {select, selectSlice} from '@rx-angular/state/selections';
-import {Location, NgFor, NgIf, NgOptimizedImage} from '@angular/common';
+import { select, selectSlice } from '@rx-angular/state/selections';
+import { Location, NgFor, NgIf, NgOptimizedImage } from '@angular/common';
 import {
   ChangeDetectionStrategy,
   Component,
@@ -9,21 +9,20 @@ import {
   ViewChild,
   ViewEncapsulation,
 } from '@angular/core';
-import {filter, map, mergeWith, tap} from 'rxjs';
-import {TMDBMovieGenreModel} from '../../data-access/api/model/movie-genre.model';
-
-import {MovieCast, MovieDetailAdapter} from './movie-detail-page.adapter';
-import {RxActionFactory} from '@rx-angular/state/actions';
-import {RxEffects} from '@rx-angular/state/effects';
-import {DetailGridComponent} from '../../ui/component/detail-grid/detail-grid.component';
-import {StarRatingComponent} from '../../ui/pattern/star-rating/star-rating.component';
-import {MovieListComponent} from '../../ui/pattern/movie-list/movie-list.component';
-import {RxLet} from '@rx-angular/template/let';
-import {BypassSrcDirective} from '../../shared/cdk/bypass-src.directive';
-import {RxFor} from '@rx-angular/template/for';
-import {FastSvgComponent} from '@push-based/ngx-fast-svg';
-import {RxIf} from '@rx-angular/template/if';
-import {RouterLink} from '@angular/router';
+import { filter, map, mergeWith, tap } from 'rxjs';
+import { TMDBMovieGenreModel } from '../../data-access/api/model/movie-genre.model';
+import { MovieCast, MovieDetailAdapter } from './movie-detail-page.adapter';
+import { RxActionFactory } from '@rx-angular/state/actions';
+import { RxEffects } from '@rx-angular/state/effects';
+import { DetailGridComponent } from '../../ui/component/detail-grid/detail-grid.component';
+import { StarRatingComponent } from '../../ui/pattern/star-rating/star-rating.component';
+import { MovieListComponent } from '../../ui/pattern/movie-list/movie-list.component';
+import { RxLet } from '@rx-angular/template/let';
+import { BypassSrcDirective } from '../../shared/cdk/bypass-src.directive';
+import { RxFor } from '@rx-angular/template/for';
+import { FastSvgComponent } from '@push-based/ngx-fast-svg';
+import { RxIf } from '@rx-angular/template/if';
+import { RouterLink } from '@angular/router';
 
 @Component({
   standalone: true,
@@ -87,7 +86,10 @@ export default class MovieDetailPageComponent {
     }>
   ) {
     this.effects.register(
-      this.ui.dialog$.pipe(map((v) => v === 'show'), tap(console.log)),
+      this.ui.dialog$.pipe(
+        map((v) => v === 'show'),
+        tap(console.log)
+      ),
       (openDialog) =>
         openDialog
           ? this.trailerDialog?.nativeElement?.showModal()
