8ff8384c690bdfa024311310312a16b2d6f767cf
Michael <michael.hladky@push-based.io>
2023-07-18 10:27:33 +0200

    fix: fix eslint errors in cloudflare-worker

---
+++ projects/cloudflare-worker/src/app/environment.token.ts
@@ -0,0 +1,18 @@
+import {InjectionToken, Provider} from '@angular/core';
+
+export interface EdgeEnvironment {
+  ASSETS: { fetch: typeof fetch };
+  NGMOVIES: any; // namespace
+}
+
+export interface EdgeEnvironmentData {
+  env: EdgeEnvironment;
+  request: Request;
+}
+
+export const EdgeEnvironmentToken = new InjectionToken<EdgeEnvironmentData>('EdgeEnv');
+
+export const provideEdgeEnvironment = ({env, request}: EdgeEnvironmentData): Provider => ({
+  provide: EdgeEnvironmentToken,
+  useValue: {env, request},
+});
