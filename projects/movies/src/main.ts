33a969d157114b570b4eafc2d9b6386fd53312c1
Michael <michael.hladky@push-based.io>
2023-07-26 18:47:37 +0200

    refactor: refactor main.js

--- projects/movies/src/main.ts
+++ projects/movies/src/main.ts
@@ -1,24 +1,9 @@
-import {AppComponent} from './app/app.component';
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {appConfig} from './app/app.config';
-import {RouterOutlet} from '@angular/router';
-import {AppShellComponent} from './app/app-shell/app-shell.component';
-import {RxLet} from '@rx-angular/template/let';
-
-@NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, RouterOutlet, AppShellComponent, RxLet],
-  providers: [...appConfig().providers],
-  bootstrap: [AppComponent],
-})
-export class AppModule {
-}
+import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
+import {AppModule} from "./app/app.module";
 
 /**
  * We have to use `platformBrowserDynamic` until Angular allows to run `bootstrapApplication` in zone-less mode
  */
 platformBrowserDynamic()
-  .bootstrapModule(AppModule, { ngZone: 'noop' })
+  .bootstrapModule(AppModule, {ngZone: 'noop'})
   .catch((err) => console.error(err));
