9da9afae9777ce5a939e803afcf23bb429c6fc98
Michael <michael.hladky@push-based.io>
2023-07-26 20:40:32 +0200

    refactor: wip7

--- projects/test-selectors/src/lib/fixtures/sidebar.ts
+++ projects/test-selectors/src/lib/fixtures/sidebar.ts
@@ -1,11 +1,9 @@
-import {CategoryNames, GenreIds} from '../internals/typings';
-
 export const sideMenuBtnSelector = '*[data-uf="menu-btn"]';
 
-export const categorySelector = (c: CategoryNames): string => {
+export const categorySelector = (c: string): string => {
   return `*[data-uf="menu-cat-${c}"]`;
 };
 
-export const genreSelector = (g: GenreIds): string => {
+export const genreSelector = (g: string): string => {
   return `*[data-uf="menu-gen-${g}"]`;
 };
