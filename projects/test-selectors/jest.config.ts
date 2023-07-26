c6c025a01c7afdc2260ffd587939a5da0b07a5f5
Michael <michael.hladky@push-based.io>
2023-07-26 21:14:00 +0200

    refactor: wip12

--- projects/test-selectors/jest.config.ts
+++ projects/test-selectors/jest.config.ts
@@ -5,18 +5,18 @@ export default {
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   coverageDirectory: '../../coverage/test-selectors',
   transform: {
-    '^.+\\.(ts|mjs|js|html)$': [
+   /* '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
       {
         tsconfig: '<rootDir>/tsconfig.spec.json',
         stringifyContentPathRegex: '\\.(html|svg)$',
       },
-    ],
+    ],*/
   },
   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   snapshotSerializers: [
-    'jest-preset-angular/build/serializers/no-ng-attributes',
-    'jest-preset-angular/build/serializers/ng-snapshot',
-    'jest-preset-angular/build/serializers/html-comment',
+    /*  'jest-preset-angular/build/serializers/no-ng-attributes',
+      'jest-preset-angular/build/serializers/ng-snapshot',
+      'jest-preset-angular/build/serializers/html-comment',*/
   ],
 };
