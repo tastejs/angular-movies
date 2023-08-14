69523c402ac060c68bbff8523749bf184bbda479
Michael <michael.hladky@push-based.io>
2023-08-14 19:06:11 +0200

    refactor: fix imports

--- projects/firebase-function/user-flows/ci-flow.uf.ts
+++ projects/firebase-function/user-flows/ci-flow.uf.ts
@@ -1,15 +1,25 @@
-import {getLhConfig} from '../../movies-user-flows/src/internals/test-sets';
+import { getLhConfig } from '../../movies-user-flows/src/internals/test-sets';
 
-import {UserFlowContext, UserFlowInteractionsFn, UserFlowOptions, UserFlowProvider,} from '@push-based/user-flow';
-import {mergeBudgets, MovieDetailPageUFO, MovieListPageUFO, SidebarUFO,} from '../../movies-user-flows/src';
+import {
+  UserFlowContext,
+  UserFlowInteractionsFn,
+  UserFlowOptions,
+  UserFlowProvider,
+} from '@push-based/user-flow';
+import {
+  mergeBudgets,
+  MovieDetailPageUFO,
+  MovieListPageUFO,
+  SidebarUFO,
+} from '../../movies-user-flows/src';
 
 import Budget from 'lighthouse/types/lhr/budget';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const angularBudgets: Budget[] = require('../../testing/budgets/angular.budgets.json');
+const angularBudgets: Budget[] = require('../../movies/testing/budgets/angular.budgets.json');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const generalTimingBudget: Budget[] = require('../../testing/budgets/general-timing.budgets.json');
+const generalTimingBudget: Budget[] = require('../../movies/testing/budgets/general-timing.budgets.json');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const movieListBudgets: Budget[] = require('../../testing/budgets/movie-list.budgets.json');
+const movieListBudgets: Budget[] = require('../../movies/testing/budgets/movie-list.budgets.json');
 
 const flowOptions: UserFlowOptions = {
   name: 'Firebase Function Emulation',
@@ -18,7 +28,7 @@ const flowOptions: UserFlowOptions = {
 const interactions: UserFlowInteractionsFn = async (
   context: UserFlowContext
 ): Promise<any> => {
-  const {flow, collectOptions} = context;
+  const { flow, collectOptions } = context;
   const url = `${collectOptions.url}/list/category/popular`;
   const sidebar = new SidebarUFO(context);
   const movieListPage = new MovieListPageUFO(context);
