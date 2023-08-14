0435897c4577ca5ce97d632df849b7726b8fceb9
Vojtech Mašek <vojtech@flowup.cz>
2023-08-14 13:24:03 +0200

    refactor(ng-universal-express): use path resolve for icons on SSR read file

--- projects/ng-universal-express/src/app/icon-load.ssr.strategy.ts
+++ projects/ng-universal-express/src/app/icon-load.ssr.strategy.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { SvgLoadStrategy } from '@push-based/ngx-fast-svg';
 import { readFileSync } from 'node:fs';
-import { of } from 'rxjs';
+import { resolve } from 'node:path';
+import { Observable, of } from 'rxjs';
 
 @Injectable()
 export class IconLoadStrategySsr implements SvgLoadStrategy {
-  load(url: string) {
-    return of(readFileSync(url, 'utf8'));
+  load(url: string): Observable<string> {
+    return of(readFileSync(resolve(url), 'utf8'));
   }
 }
