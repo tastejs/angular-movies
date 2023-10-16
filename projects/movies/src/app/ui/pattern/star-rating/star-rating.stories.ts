d018fb815918adcabe29836bcd199816a5f81d40
ChristopherPHolder <toholderandrews@gmail.com>
2023-10-16 23:45:36 +0200

    chore(movies): add args type to star rating

--- projects/movies/src/app/ui/pattern/star-rating/star-rating.stories.ts
+++ projects/movies/src/app/ui/pattern/star-rating/star-rating.stories.ts
@@ -1,10 +1,36 @@
 import { Meta, StoryObj } from '@storybook/angular';
 import { StarRatingComponent } from './star-rating.component';
+import {
+  withBothColorScheme,
+  wrappedInPaddedDiv,
+} from '../../../../../.storybook/decorators';
 
 const meta: Meta = {
   title: 'Patterns/Star Rating',
   component: StarRatingComponent,
   tags: ['autodocs'],
+  decorators: [wrappedInPaddedDiv, withBothColorScheme],
+  args: {
+    showRating: false,
+  },
+  argTypes: {
+    rating: {
+      control: {
+        type: 'number',
+        min: 0,
+        max: 10,
+        step: 0.1,
+      },
+      description:
+        'A rating as a number from 1 to 10 which decided the number of stars',
+    },
+    showRating: {
+      control: {
+        type: 'boolean',
+      },
+      description: 'Weather or not to show the rating as a number',
+    },
+  },
 };
 export default meta;
 type Story = StoryObj<StarRatingComponent>;
