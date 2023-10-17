055ea6e11f89e176e60058c4e93ab7e754063a56
ChristopherPHolder <toholderandrews@gmail.com>
2023-10-17 23:53:57 +0200

    chore(movies): remove comments

--- projects/movies/.storybook/main.ts
+++ projects/movies/.storybook/main.ts
@@ -19,6 +19,3 @@ const config: StorybookConfig = {
 
 export default config;
 
-// To customize your webpack configuration you can use the webpackFinal field.
-// Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
-// and https://nx.dev/packages/storybook/documents/custom-builder-configs
