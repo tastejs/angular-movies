108b2d2784992a0c3780e7bb61300d1bc5c95ea6
Michael <michael.hladky@push-based.io>
2023-11-12 19:56:06 +0100

    refactor(movies): fix effect

--- projects/movies/src/app/ui/component/dark-mode-toggle/dark-mode-toggle.component.ts
+++ projects/movies/src/app/ui/component/dark-mode-toggle/dark-mode-toggle.component.ts
@@ -1,8 +1,13 @@
-import {DOCUMENT} from '@angular/common';
-import {ChangeDetectionStrategy, Component, inject, ViewEncapsulation,} from '@angular/core';
-import {RxLet} from '@rx-angular/template/let';
-import {rxState} from '@rx-angular/state';
-import {rxEffects} from "@rx-angular/state/effects";
+import { DOCUMENT } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  ViewEncapsulation,
+} from '@angular/core';
+import { RxLet } from '@rx-angular/template/let';
+import { rxState } from '@rx-angular/state';
+import { rxEffects } from '@rx-angular/state/effects';
 
 @Component({
   standalone: true,
@@ -48,14 +53,15 @@ import {rxEffects} from "@rx-angular/state/effects";
   encapsulation: ViewEncapsulation.Emulated,
 })
 export class DarkModeToggleComponent {
-  state = rxState<{ isLightTheme: boolean }>(({set}) => {
+  state = rxState<{ isLightTheme: boolean }>(({ set }) => {
     set({ isLightTheme: true });
   });
   isLightTheme$ = this.state.select('isLightTheme');
-  effects = rxEffects(e => e.
-  register(this.isLightTheme$, this.toggleTheme));
   private readonly document = inject(DOCUMENT);
 
+  constructor() {
+    rxEffects((e) => e.register(this.isLightTheme$, this.toggleTheme));
+  }
   toggleTheme = (isLightTheme: boolean): void => {
     if (isLightTheme) {
       this.document.body.classList.remove('dark');
