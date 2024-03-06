ffde2740ea2c9c72b1ecc534c355dc82ce38af0c
Vojtech Mašek <vojtech@flowup.cz>
2024-03-06 10:08:47 +0100

    refactor(movies): remove local run that executes by default and messes up functions serving

--- projects/movies/server.ts
+++ projects/movies/server.ts
@@ -55,15 +55,3 @@ export default function app(): express.Express {
 
   return server;
 }
-
-function run(): void {
-  const port = process.env['PORT'] || 4000;
-
-  // Start up the Node server
-  const server = app();
-  server.listen(port, () => {
-    console.log(`Node Express server listening on http://localhost:${port}`);
-  });
-}
-
-run();
