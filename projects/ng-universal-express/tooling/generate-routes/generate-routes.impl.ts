1ee6ec2c8f28a9da7605d98dbfc7df5f6fd467dc
Julian Jandl <julian@jandl.wien>
2024-08-29 22:10:28 +0200

    chore: fix ci

--- projects/ng-universal-express/tooling/generate-routes/generate-routes.impl.ts
+++ projects/ng-universal-express/tooling/generate-routes/generate-routes.impl.ts
@@ -1,17 +1,25 @@
-import {existsSync, mkdirSync, readFileSync, WriteFileOptions, writeFileSync,} from 'node:fs';
-import {dirname} from 'node:path';
-import {EOL} from 'node:os';
+import {
+  existsSync,
+  mkdirSync,
+  readFileSync,
+  WriteFileOptions,
+  writeFileSync,
+} from 'node:fs';
+import { dirname } from 'node:path';
+import { EOL } from 'node:os';
 
-import {TMDBMovieModel} from '../../../movies/src/app/data-access/api/model/movie.model';
-import {GenresResponse} from '../../../movies/src/app/data-access/api/resources/genre.resource';
-import {environment} from '../../../movies/src/environments/environment';
-import {getLog} from "../utils";
+import { TMDBMovieModel } from '../../../movies/src/app/data-access/api/model/movie.model';
+import { GenresResponse } from '../../../movies/src/app/data-access/api/resources/genre.resource';
+import { environment } from '../../../movies/src/environments/environment';
+import { getLog } from '../utils';
 
-
-export function run(parameters: { targetFile: string, sourceFile?: string, verbose?: boolean }): Promise<void> {
-
+export function run(parameters: {
+  targetFile: string;
+  sourceFile?: string;
+  verbose?: boolean;
+}): Promise<void> {
   // setup
-  let {targetFile, sourceFile, verbose} = parameters;
+  let { targetFile, sourceFile, verbose } = parameters;
 
   const log = getLog(verbose);
 
@@ -22,7 +30,6 @@ export function run(parameters: { targetFile: string, sourceFile?: string, verbo
 
   let defaultRoutes: string[] = [];
 
-
   if (sourceFile && existsSync(sourceFile)) {
     log(`Read source rout.txt ${sourceFile}`);
     defaultRoutes = readFileSync(sourceFile)
@@ -32,7 +39,7 @@ export function run(parameters: { targetFile: string, sourceFile?: string, verbo
     log(`Default routes: ${defaultRoutes}`);
   }
 
-// URLs
+  // URLs
 
   const movieGenresURL = readApi('genre/movie/list');
   const genresListURL = (id: string | number) => readApi(`list/genre/${id}`);
@@ -41,47 +48,42 @@ export function run(parameters: { targetFile: string, sourceFile?: string, verbo
 
   const movieGenresRoutes = _fetch<{ genres: GenresResponse }>(movieGenresURL, {
     headers: getTmdbHeaders(),
-  })
-    // eslint-disable-next-line unicorn/prefer-top-level-await
-    .then(({genres}) => {
-      return genres.map(({id}) => genresListURL(id))
-    })
+  }).then(({ genres }) => {
+    return genres.map(({ id }) => genresListURL(id));
+  });
 
-// how many page details of popular movies should be pre-rendered
-// @ts-ignore
+  // how many page details of popular movies should be pre-rendered
+  // @ts-ignore
   const moviesPopularRoutes = (options: { pages: number }) => {
-    return Array.from({length: options.pages}, (_, index) => {
+    return Array.from({ length: options.pages }, (_, index) => {
       return _fetch<{ results: TMDBMovieModel[] }>(moviesPopularURL, {
         headers: getTmdbHeaders(),
         params: {
           sort_by: 'popularity.asc',
           page: index + 1,
         },
-      }).then(({results}) => results.map(({id}) => movieDetailURL(id)))
+      }).then(({ results }) => results.map(({ id }) => movieDetailURL(id)));
     });
   };
-// GENERATE
+  // GENERATE
   return Promise.all([
     Promise.resolve(defaultRoutes),
     movieGenresRoutes, // list the routes of the genres featured in the app sidebar
     // list routes for movie details equivalent to N pages of popular movies list
-    ...moviesPopularRoutes({pages: 2}),
+    ...moviesPopularRoutes({ pages: 2 }),
   ])
     .then((routes) => {
       const normalizedRoutes = routes
         .flat()
         .flatMap(
-          (route) => route.replace('https://api.themoviedb.org/3', '') as string
-        )
+          (route) =>
+            route.replace('https://api.themoviedb.org/3', '') as string,
+        );
 
-      const content = normalizedRoutes.join(EOL)
+      const content = normalizedRoutes.join(EOL);
       log(`Content: ${content}`);
-      writeFileSyncRecursive(
-        targetFile,
-        content
-      );
+      writeFileSyncRecursive(targetFile, content);
     })
-    // eslint-disable-next-line unicorn/prefer-top-level-await
     .catch((error) => console.error(error));
 }
 
@@ -89,14 +91,13 @@ function readApi(url: string): string {
   return `${environment.tmdbBaseUrl}/${environment.apiV3}/${url}`;
 }
 
-// eslint-disable-next-line unicorn/no-object-as-default-parameter
 function writeFileSyncRecursive(
   filename: string,
   content: string,
-  options?: WriteFileOptions
+  options?: WriteFileOptions,
 ): void {
-  const options_ = options || {encoding: 'utf8'};
-  mkdirSync(dirname(filename), {recursive: true});
+  const options_ = options || { encoding: 'utf8' };
+  mkdirSync(dirname(filename), { recursive: true });
   writeFileSync(filename, content, options_);
 }
 
@@ -109,9 +110,14 @@ function getTmdbHeaders() {
   };
 }
 
-function _fetch<T>(url: string, f: RequestInit & { params?: Record<string, any> }) {
-  const {params, ...fetchRequestInit} = f
+function _fetch<T>(
+  url: string,
+  f: RequestInit & { params?: Record<string, any> },
+) {
+  const { params, ...fetchRequestInit } = f;
   const parametersAsString = new URLSearchParams(params).toString();
   const urlToFetch = `${url}${parametersAsString ? '?' + parametersAsString : ''}`;
-  return fetch(urlToFetch, fetchRequestInit).then((response: Response) => response.json()) as Promise<T>
+  return fetch(urlToFetch, fetchRequestInit).then((response: Response) =>
+    response.json(),
+  ) as Promise<T>;
 }
